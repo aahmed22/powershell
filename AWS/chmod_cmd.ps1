aad1789371f5015d44cfe6f741fb08a4e472afe6
Abdul <abdulhamid23@gmail.com>
2021-03-18 12:38:15 -0400

    Create chmod_cmd.ps1

---
+++ AWS/chmod_cmd.ps1
@@ -0,0 +1,9 @@
+# Executing pem via powershell 
+
+$path = ".\NameOfPemKey.pem"
+# Reset to remove explict permissions
+icacls.exe $path /reset
+# Give current user explicit read-permission
+icacls.exe $path /GRANT:R "$($env:USERNAME):(R)"
+# Disable inheritance and remove inherited permissions
+icacls.exe $path /inheritance:r
